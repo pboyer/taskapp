1b636bfc6b3afc8c2be1e336891d2f88adfa4eb9
Peter Boyer <peter.b.boyer@gmail.com>
2017-07-31 14:34:42 -0400

    Add auth lambda

---
+++ functions/auth/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	apex "github.com/apex/go-apex"
+)
+
+type request struct {
+	authorizationToken *string `json:"authorizationToken"`
+}
+
+func main() {
+	apex.HandleFunc(func(event json.RawMessage, actx *apex.Context) (interface{}, error) {
+		var req request
+
+		if err := json.Unmarshal(event, &req); err != nil {
+			return nil, err
+		}
+
+		// lookup username
+		return nil, fmt.Errorf("Not implemented. Input: %v", req)
+	})
+}
